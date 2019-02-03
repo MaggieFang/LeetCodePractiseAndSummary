daf8fc8fef8116cd80ae248f942a72a912e50497
Maggie Fang <menogenfong@gmail.com>
2019-02-03 15:59:42 -0500

    spiral matrix

---
+++ java-solution/src/com/doordash/BatchDelivery.java
@@ -0,0 +1,16 @@
+package com.doordash;
+
+/**
+ * Author by Maggie Fang. Email maggie2fang@gmail.com. Date on 2019-02-03
+ * Talk is Cheap,Show me the Code.
+ **/
+public class BatchDelivery {
+    // 1. pick and drop should be pair. 2. pick should before drop. 3.cannot pick the same one two times
+    //my mind: use a hashset to put the pair one and compare .e.g.meet p1, out d1 into the pair. when meet d1. judge whethere in the set.
+    // another one set to put the ones finished.
+    public boolean valid(String s){
+
+        return false;
+
+    }
+}
---
+++ java-solution/src/com/google/SpiralMatrix54M.java
@@ -0,0 +1,68 @@
+package com.google;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author by Maggie Fang. Email maggie2fang@gmail.com. Date on 2019-02-03
+ * Talk is Cheap,Show me the Code.
+ **/
+public class SpiralMatrix54M {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new ArrayList<>();
+        }
+        int sm = 0, sn = 0;
+        int m = matrix.length - 1;
+        int n = matrix[0].length - 1;
+        List<Integer> ans = new ArrayList<>();
+        while (sm <= m && sn <= n) {
+            // row: left to right;
+            if (sm <= m) { // if there are rows left to print
+                int i = sn;
+                while (i <= n) {
+                    ans.add(matrix[sm][i++]);
+                }
+                sm++;
+            }
+
+            //column: top-down
+            if (sn <= n) {
+                int i = sm;
+                while (i <= m) {
+                    ans.add(matrix[i++][n]);
+                }
+
+                n--;
+            }
+
+            //row: right to left;
+            if (sm <= m) {// if there are rows left to print
+                int i = n;
+                while (i >= sn) {
+                    ans.add(matrix[m][i--]);
+                }
+                m--;
+            }
+
+            if (sn <= n) {
+                int i = m;
+                while (i >= sm) {
+                    ans.add(matrix[i--][sn]);
+                }
+                sn++;
+            }
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        SpiralMatrix54M s = new SpiralMatrix54M();
+        s.spiralOrder(new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        });
+    }
+}
