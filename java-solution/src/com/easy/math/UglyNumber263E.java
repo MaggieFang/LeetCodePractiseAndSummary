c54be0e1c16acd2d94994f53db2292145457a314
Maggie Fang <menogenfong@gmail.com>
2019-05-09 21:08:29 -0400

    ugly number

---
+++ java-solution/src/com/easy/math/UglyNumber263E.java
@@ -0,0 +1,25 @@
+package com.easy.math;
+
+/**
+ * Author by Maggie Fang. Email maggie2fang@gmail.com. Date on 2019-05-09
+ * Talk is cheap,show me the Code.
+ **/
+public class UglyNumber263E {
+    public boolean isUgly(int num) {
+        if (num == 0) return false;
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else if (num % 3 == 0) {
+                num = num / 3;
+            } else if (num % 5 == 0) {
+                num = num / 5;
+            } else {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+}
---
+++ java-solution/src/com/easy/math/UglyNumberII264M.java
@@ -0,0 +1,36 @@
+package com.easy.math;
+
+import java.util.TreeSet;
+
+/**
+ * Author by Maggie Fang. Email maggie2fang@gmail.com. Date on 2019-05-09
+ * Talk is cheap,show me the Code.
+ **/
+public class UglyNumberII264M {
+    /**
+     * Clarification:
+     *
+     * </p>
+     * Keypoints: use a treeSet, from the most basic element 2,3,5,
+     * for every element,when we multiply 2,3,5 on it. it also meet ugly number definition
+     * so we can use a treest, initiate with 1, and pop it out to multiply 2,3,5 and add it to set.
+     * when we do n times pop. we first element is our answer.
+     *
+     * </p>
+     * TIME COMPLEXITY: O(n)
+     * SPACE COMPLEXITY: O(n)
+     * </p>
+     **/
+    public int nthUglyNumber(int n) {
+        TreeSet<Long> set = new TreeSet<>();
+        set.add(1L);
+        for (int i = 1; i < n; i++) {
+            long l = set.pollFirst();
+            set.add(l * 2);
+            set.add(l * 3);
+            set.add(l * 5);
+        }
+        return set.first().intValue();
+
+    }
+}
