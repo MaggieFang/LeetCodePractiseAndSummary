c8e9bca7f1bfc9fc77bbf267e1304e825ea42c36
Maggie Fang <menogenfong@gmail.com>
2019-09-02 22:21:50 -0400

    update

--- java-solution/src/com/mianjing/gou/InsertInterval57H.java
+++ java-solution/src/com/mianjing/gou/InsertInterval57H.java
@@ -2,6 +2,7 @@ package com.mianjing.gou;
 
 import com.basicStructure.Interval;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,8 +11,49 @@ import java.util.List;
  * Talk is Cheap,Show me the Code.
  **/
 public class InsertInterval57H {
+    /**
+     * Clarification:
+     *  test cases:
+     *  new int is less than all, e.g newIn = [0,0], int={[5,7]}
+     *  new int is larger than all
+     *  new int cross some
+     * </p>
+     * Keypoints:
+     *
+     * </p>
+     * TIME COMPLEXITY: O(n)
+     * SPACE COMPLEXITY:O(n)
+     * </p>
+     **/
+    public int[][] insert(int[][] in, int[] newIn) {
+        if (in == null || in.length == 0) return new int[][]{newIn};
+
+        List<int[]> list = new ArrayList<>();
+        int i = 0;
+        /// add all the intervals ending before newInterval starts
+        while (i < in.length && in[i][1] < newIn[0]) {
+            list.add(in[i++]);
+        }
+
+        // merge all overlapping intervals to one considering newInterval
+        while (i < in.length && in[i][0] <= newIn[1]) {
+            // do the update here. e.g [5,7], newIn [0,0]
+            newIn[0] = Math.min(newIn[0], in[i][0]);
+            newIn[1] = Math.max(newIn[1], in[i][1]);
+            i++;
+        }
+
+        list.add(newIn);
+        while (i < in.length) {
+            list.add(in[i++]);
+        }
+        return list.toArray(new int[list.size()][2]);
+
+
+    }
+
     //don't use new structure to return.
-    public List<Interval> insertImprove(List<Interval> list, Interval newIn) {
+    public List<Interval> insert(List<Interval> list, Interval newIn) {
         int n = list.size();
         int i = 0;
         /// add all the intervals ending before newInterval starts
@@ -26,48 +68,13 @@ public class InsertInterval57H {
             i++;
         }
 
-        int end = i-1;
-        for(int x = end; x >= begin; x--){
+        int end = i - 1;
+        for (int x = end; x >= begin; x--) {
             list.remove(x);
         }
         // add the union of intervals we got
-        list.add(begin,newIn);
+        list.add(begin, newIn);
         return list;
 
-    }
-
-    public List<Interval> insert(List<Interval> list, Interval newIn) {
-        int n = list.size();
-        List<Interval> ans = new LinkedList<>();
-        int i = 0;
-        /// add all the intervals ending before newInterval starts
-        while (i < n && list.get(i).end < newIn.start) {
-            ans.add(list.get(i));
-            i++;
-        }
-        // merge all overlapping intervals to one considering newInterval
-        while (i < n && newIn.end >= list.get(i).start) {
-            newIn.start = Math.min(newIn.start, list.get(i).start);
-            newIn.end = Math.max(newIn.end, list.get(i).end);
-            i++;
-        }
-        // add the union of intervals we got
-        ans.add(newIn);
-        // add all the rest
-        while (i < n) {
-            ans.add(list.get(i++));
-        }
-        return ans;
-
-    }
-
-    public static void main(String[] args){
-        InsertInterval57H t = new InsertInterval57H();
-        List<Interval> list = new LinkedList<>();
-        list.add(new Interval(0,2));
-        t.insertImprove(list,new Interval(3,4));
-        for(Interval i : list){
-            System.out.print("["+i.start+","+i.end+"]\t");
-        }
     }
 }
