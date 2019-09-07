01c33670525c16371ccf0352ec863cc56adf8d57
Maggie Fang <menogenfong@gmail.com>
2019-09-07 19:55:05 -0400

    update

--- java-solution/src/com/mianjing/gou/DecodeString394M.java
+++ java-solution/src/com/mianjing/gou/DecodeString394M.java
@@ -11,46 +11,42 @@ public class DecodeString394M {
      * every time when you meet a number,indicate num, it must be followed by [...],
      * we just need to recursively call our method to decode "..." and gets sub result,
      * then repeat the sub result "num" times.
+     * so we beging from i = 0, there are some characters here, 1)'[' must  after number, so what we met first must be letter or number.
+     * if it is letter, we append to the result, if it is number, we loop the read the number, then after the number, it must be a '[',
+     * we skip it and call recur. and the end  condition is we go to the end of the string or meet ']'
      * </p>
      * TIME COMPLEXITY:
      * <p>
      * SPACE COMPLEXITY:
      * <p>
      **/
+    private int i = 0;
     public String decodeString(String s) {
-        if (s == null || s.length() == 0) return "";
-        StringBuilder sb = new StringBuilder();
-
+        if(s == null || s.length() == 0) return "";
+        return decode(s).toString();
+    }
+    private StringBuilder decode(String s){
+        StringBuilder ans = new StringBuilder();
         int n = s.length();
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
+        char c;
+        while(i < n &&  (c = s.charAt(i++)) != ']'){ // end condition
+            if(Character.isDigit(c)){ // meet digital
                 int num = c - '0';
-                i++;
-                while (i < n && s.charAt(i) != '[') {
-                    num = num * 10 + s.charAt(i) - '0';
+                while(Character.isDigit(c = s.charAt(i))){
+                    num = num *10 + c - '0';
                     i++;
                 }
-                i++;
-                int start = i;
-                int cnt = 1;
-                while (cnt != 0) {
-                    if (s.charAt(i) == '[') cnt++;
-                    else if (s.charAt(i) == ']') cnt--;
-                    i++;
+                ++i;//after digital, it must be a '['
+                StringBuilder sub = decode(s); //
+                while(num--> 0){
+                    ans.append(sub);
                 }
-                --i;
-                int end = i;
-                String sub = decodeString(s.substring(start, end));
-                for (int k = 0; k < num; k++) {
-                    sb.append(sub);
-                }
-            } else {
-                sb.append(c);
+
+            }else{ // meet direct letter
+                ans.append(c);
             }
         }
-        return sb.toString();
-
+        return ans;
     }
 
 }
