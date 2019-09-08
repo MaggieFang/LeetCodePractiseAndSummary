9137f616a099e86283e6a5d4ef7b04c4e55975db
Maggie Fang <menogenfong@gmail.com>
2019-09-08 15:55:32 -0400

    min priorityQueue for merging K sorted link lists

--- java-solution/src/com/easy/linkedlist/MergeTwoSortedLink21.java
+++ java-solution/src/com/easy/linkedlist/MergeTwoSortedLink21.java
@@ -32,7 +32,7 @@ public class MergeTwoSortedLink21 {
 
         ListNode preHead = new ListNode(0);
         ListNode node  = preHead;
-        while(l1 != null && l2 != null){
+        while(l1 != null && l2 != null){// 分成这样&&跟后面的单独拼接两步 比用 || 好一些，毕竟后面拼接不用一个个遍历
             int v1 = l1.val;
             int v2 = l2.val;
             if(v1 < v2){
@@ -45,14 +45,16 @@ public class MergeTwoSortedLink21 {
                 node = node.next;
             }
         }
+
         if(l1 != null){
-            node.next = l1;
+            node.next = l1;// joint directly
         }
         if(l2 != null){
             node.next = l2;
         }
         return preHead.next;
     }
+
 
     public ListNode mergeTwoListsRecurrence(ListNode l1, ListNode l2) {
         if (l1 == null) {
--- java-solution/src/com/fb/MergeKSortedList23H.java
+++ java-solution/src/com/fb/MergeKSortedList23H.java
@@ -12,52 +12,49 @@ import java.util.PriorityQueue;
 public class MergeKSortedList23H {
     /**
      * KEYPOINTS:
-     * <p>
      * 1->4->5
-     * <p>
      * 1->3->8
-     * <p>
      * 2->6
      * how we do? we will pointer to the head of each. and get its minimum. it will be the head of result.
      * and after we choose one.we move its pointer to next. and compare the n numbers again to pick the minimum
      * keep doing so util each point to the last element.
-     * <p>
      * so Min_Heap can help us to do that.
-     * <p>
      * we first and the list[i] to the heap.
-     * <p>
-     * </p>
      * PSEUDOCODE:
      * <pre>
      *  <code>
      *
      * </code>
      * </pre>
-     * TIME COMPLEXITY:O(NlogK), every time to put in a queue ,we need logK to sort the min_heap.and we do N times
+     * TIME COMPLEXITY:O(NlogK), K is the size of list, N is the total nodes .
+     * every time to put in a queue ,we need logK to sort the min_heap.and we do N times
      * <p>
-     * SPACE COMPLEXITY: O(k)
+     * SPACE COMPLEXITY: O(k) K is the size of list
      * <p>
      **/
     public ListNode mergeKLists(ListNode[] list) {
         if (list == null || list.length == 0) {
             return null;
         }
-        PriorityQueue<ListNode> queue = new PriorityQueue<>(list.length, new Comparator<ListNode>() {
-            @Override
-            public int compare(ListNode o1, ListNode o2) {
-                return o1.val - o2.val;
-            }
-        });
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] != null) {
-                queue.add(list[i]);
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((ListNode o1, ListNode o2) -> o1.val - o2.val);
+        for (ListNode node : list) {
+            if (node != null) {
+                queue.add(node);
             }
         }
-
+        
         ListNode head = new ListNode(0); // avoid null judgement in the while
         ListNode cur = head;
         while (!queue.isEmpty()) {
             ListNode node = queue.poll();
+//            for(ListNode i : list){
+//                if(node == i){
+//                    i = i.next; //这么做是不行的，没有把他赋值回去list[i]
+//                    if(i != null){
+//                        queue.add(i);
+//                    }
+//                }
+//            }
             for (int i = 0; i < list.length; i++) {
                 if (node == list[i]) {
                     list[i] = list[i].next;
@@ -72,4 +69,6 @@ public class MergeKSortedList23H {
         }
         return head.next;
     }
+
+
 }
