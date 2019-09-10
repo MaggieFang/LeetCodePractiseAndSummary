56f48a0488a9402db97a36ae34e80043e1886a2c
Maggie Fang <menogenfong@gmail.com>
2019-09-10 00:13:32 -0400

    二刷

--- java-solution/src/com/easy/string/ValidParenthese20.java
+++ java-solution/src/com/easy/string/ValidParenthese20.java
@@ -7,8 +7,41 @@ import java.util.Stack;
  * Talk is Cheap,Show me the Code.
  **/
 public class ValidParenthese20 {
-    public static void main(String[] args) {
-        System.out.println(isValidTrick("()[]{}"));
+    /**
+     * KEYPOINTS:
+     * <p>
+     * a very good idea compare to the above solution.
+     * When meet open blanket, just to push the close blanket that the open blanket need latter.
+     * When meet close blanket, pop if it is the same with it.
+     * if stack.isEmpty()|| stack.pop() != c return false.e.g case s=']', the stack is empty
+     * and finally return stack.isEmpty(); e.g case s='[' we push a ']' in stack
+     * </p>
+     * PSEUDOCODE:
+     * <pre>
+     *  <code>
+     *
+     * </code>
+     * </pre>
+     * TIME COMPLEXITY:
+     * <p>
+     * SPACE COMPLEXITY:
+     * <p>
+     **/
+    public static boolean isValidTrick(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '['){
+                stack.push(']');
+            }else if(c == '('){
+                stack.push(')');
+            }else if(c =='{'){
+                stack.push('}');
+            }else if(stack.isEmpty()|| stack.pop() != c){ // take care of this judge. e.g case ']',
+                return false;
+            }
+        }
+        return stack.isEmpty(); // case s = '['
     }
 
     public static boolean isValid(String s) {
@@ -67,56 +100,5 @@ public class ValidParenthese20 {
         return stack.isEmpty(); // maybe only open blankets "))"
     }
 
-    /**
-     * KEYPOINTS:
-     * <p>
-     * a very good idea compare to the above solution.
-     * When meet open blanket, just to push the close blanket that the open blanket need latter.
-     * When meet close blanket, pop if it is the same with it.
-     *
-     * </p>
-     * PSEUDOCODE:
-     * <pre>
-     *  <code>
-     *
-     * </code>
-     * </pre>
-     * TIME COMPLEXITY:
-     * <p>
-     * SPACE COMPLEXITY:
-     * <p>
-     **/
-    public static boolean isValidTrick(String s) {
-        if (s == null || s.length() < 1) {
-            return true;
-        }
 
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-
-        Stack<Character> stack = new Stack<>();
-        int i = 0;
-        while (i < s.length()) {
-            char c = s.charAt(i);
-            switch (c) {
-                case '(':
-                    stack.push(')');
-                    break;
-                case '{':
-                    stack.push('}');
-                    break;
-                case '[':
-                    stack.push(']');
-                    break;
-                default:
-                    if (stack.isEmpty() || stack.pop() != c) {
-                        return false;
-                    }
-            }
-            ++i;
-        }
-        return stack.isEmpty();
-
-    }
 }
