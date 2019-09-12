0c7c1d1c47815e40fb7ab2f5de9d971daf4a2863
Maggie Fang <menogenfong@gmail.com>
2019-09-12 13:54:07 -0400

    during computing maxDepth of Binary tree. we can find leaves indexs.

--- java-solution/src/com/easy/tree/MaxDepthOfBT104.java
+++ java-solution/src/com/easy/tree/MaxDepthOfBT104.java
@@ -6,14 +6,7 @@ package com.easy.tree;
  **/
 public class MaxDepthOfBT104 {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-
+        if (root == null) return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-
     }
 }
---
+++ java-solution/src/com/linkedin/FindLeavesofBinaryTree366M.java
@@ -0,0 +1,49 @@
+package com.linkedin;
+
+import com.easy.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author by Maggie Fang <maggie2fang@gmail.com>. Date on 2019-09-12
+ **/
+public class FindLeavesofBinaryTree366M {
+    /**
+     * Clarification:
+     *
+     *            1
+     *          / \
+     *         2   3
+     *        / \
+     *       4   5
+     *
+     * Output: [[4,5,3],[2],[1]]
+     * </p>
+     * Keypoints:
+     * refer to MaxDepthOfBT104, this can be down to find  the depth for each node. the depth is the result index this node should be put
+     * given above example, for 1, the depth 3, it should be in res[2], for 2, the depth is 1, and for 3 is 0,so it should be in res[0],...
+     * so we can refer to the MaxDepthOfBT104, to find the depth, durning the recur, we store the result for each depth.
+     *
+     * </p>
+     * TIME COMPLEXITY: O(n)
+     * SPACE COMPLEXITY: O(n)
+     * </p>
+     **/
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        maxDepth(root, list);
+        return list;
+
+    }
+
+    private int maxDepth(TreeNode root, List<List<Integer>> res) {
+        if (root == null) return -1;
+        int depth = 1 + Math.max(maxDepth(root.left, res), maxDepth(root.right, res));
+        if (depth >= res.size()) {
+            res.add(new ArrayList<>());
+        }
+        res.get(depth).add(root.val);
+        return depth;
+    }
+}
