882a98699c25565b5ace6a356b27e6c2ec1cc5f6
Maggie Fang <menogenfong@gmail.com>
2019-09-25 21:38:31 -0400

    二刷

--- java-solution/src/com/google/BraceExpansion1087M.java
+++ java-solution/src/com/google/BraceExpansion1087M.java
@@ -10,6 +10,10 @@ public class BraceExpansion1087M {
     /**
      * Clarification:
      * backtracking
+     * <p>
+     * case 1: "abcd", so need to S.indexOf('{')
+     * case 2: {a,b}
+     * case 3: ..
      * </p>
      * Keypoints:
      *
@@ -20,8 +24,9 @@ public class BraceExpansion1087M {
      **/
     public String[] expand(String S) {
         if (S == null) return new String[]{};
-        if (S.indexOf('{') < 0) return new String[]{S}; // e.g abacd
+        if (S.indexOf('{') < 0) return new String[]{S}; //!!! take care. e.g abacd
 
+        //also can use S.split("\\{|\\}"); to split. but need the eliminate the empty "" result and the "," in each string
         List<char[]> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < S.length(); i++) {
@@ -63,4 +68,27 @@ public class BraceExpansion1087M {
         }
 
     }
+
+//    public String[] expand(String S) {
+//        if (S == null || S.length() == 0) return new String[]{};
+//        String[] sp = S.split("\\{|\\}");
+//        List<char[]> tmp = new ArrayList<>();
+//        for (String s : sp) {
+//            if (s.length() == 0) continue;
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < s.length(); i++) {
+//                char c = s.charAt(i);
+//                if (c != ',') {
+//                    sb.append(c);
+//                }
+//            }
+//            tmp.add(toSortedChar(sb));
+//        }
+//        List<String> res = new ArrayList<>();
+//        dfs(tmp, 0, new StringBuilder(), res);
+//        String[] ans = new String[res.size()];
+//        ans = res.toArray(ans);
+//        return ans;
+//    }
+
 }
