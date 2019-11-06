48715022d3a2dce7aa8d8495613b8f953dcf409a
Maggie Fang <menogenfong@gmail.com>
2019-11-06 18:30:07 -0500

    comments.

--- java-solution/src/com/EventCounter.java
+++ java-solution/src/com/EventCounter.java
@@ -7,12 +7,25 @@ import java.util.Queue;
  * Author by Maggie Fang <maggie2fang@gmail.com>. Date on 2019-11-06
  **/
 public class EventCounter {
-    private int n;
+    /**
+     * Clarification:
+     *
+     * </p>
+     * Keypoints:
+     * Actually, I don't need to care about the count in 1 minutes!!! so don't use the queue for count!!!
+     * just push the timestamp into queue when the evertOccur called.
+     * for getCount, look from the beginning. remove the elements that out of the range of in the last n secs.
+     * </p>
+     * TIME COMPLEXITY:
+     * SPACE COMPLEXITY:
+     * </p>
+     **/
+    private long intervalMillis ;
     private Queue<Long> queue;
 
     public EventCounter(int n) {
         this.queue = new LinkedList<>();
-        this.n = n;
+        this.intervalMillis = n*1000;
     }
 
     public void eventOccur() {
@@ -23,7 +36,7 @@ public class EventCounter {
     public int getEventCount() {
         long cur = System.currentTimeMillis();
         while (!queue.isEmpty()) {
-            if ((cur - queue.peek()) / 1000 > n) {
+            if ((cur - queue.peek())  > intervalMillis) {
                 queue.poll();
             }
         }
